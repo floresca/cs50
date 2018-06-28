9b5324a8e83a6cf18de96ff0ce9d8fc782158a55
Andres <floresc.andres@intracitygeeks.org>
2018-06-28 18:39:40 -0400

    completed caesar cipher

--- Pset2/caesar/caesar.c
+++ Pset2/caesar/caesar.c
@@ -1,47 +1,55 @@
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 int main(int argc, string argv[])
 {
+    //Check if argc is any number other than 2
     if (argc != 2)
     {
+        //give an error
         return 1;
     }
 
+    //Turn the key from a string to a character
     int key = atoi(argv[1]);
+    //grab the plain text
     string plain = get_string("plaintext: ");
 
+    //print the cypher after the loop
+    printf("ciphertext: ");
+
+    //loop through plain
     for (int i = 0, k = strlen(plain); i < k; i++)
     {
+        //if its a character
+        if (isalpha(plain[i]))
+        {
+            //check if the character is uppder case
+            if (isupper(plain[i]))
+            {
+                //include the character into the cypher AFTER take away 65 to make A = 0
+                //add the key, then modulo 26, then ADD 65 to turn it back into ASCII uppercase
+                printf("%c", ((((plain[i] - 65) + key) % 26) + 65));
+            }
+            //check if it is lowercase instead
+            else if (islower(plain[i]))
+            {
+                //include the character into the cypher AFTER take away 97 to make a = 0
+                //add the key, then modulo 26, then ADD 97 to turn it back into ASCII lowercase
+                printf("%c", ((((plain[i] - 97) + key) % 26) + 97));
+            }
+        }
+        else
+        {
+            //if its not a character then add the character directly into the cypher
+            printf("%c", plain[i]);
+        }
 
     }
-
-
-    //when all that is done ask for the plaintext using get_string
-    //grab each character in the plain text and run it against the key
-        //For each character run the key
-        //turn the character into its ASCII code (or check that its good)
-        //if the character doesnt fall in the capital letters or the lower case letters then print it as is
-        //if the character key takes it over the last ASCII (Z) then loop to A
-    //when loop is done print the cypher text
-
-    //isalpha('') will return true is character is a letter
-    //isupper('') checks whether its upper case
-    //islower('') checks whether its lower case
-    //strlen() gives the string length per character
-
-    //if the ASCII is capital then -65
-    //if ASCII lowercase then -97
-    //alphabetical index loop
-        //(value + key) % 26
-    //bring result back to ASCII
-
-    eprintf("%i\n", key);
+    //add space
+    printf("\n");
 }
-
-
-//+++check that argc is no more and no less than 2
-    //+++turn the string number in argv[] into an integer using atoi (check the library)
-        //+++what if its not all numbers? cehck library
